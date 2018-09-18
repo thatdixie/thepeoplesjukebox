126ed73cecbba24d7d55829087ae13684a2d9809
microax <mgill@metaqueue.net>
2018-09-18 19:36:39 -0400

    fixed javascript in /user/index.php mockup

--- index.php
+++ index.php
@@ -1,17 +1,37 @@
 <?php
-require_once "include/view/page/index/indexIncludeFiles.php";
-require_once "include/model/AdminFaqModel.php";
+require_once "../include/view/page/index/indexIncludeFiles.php";
 
 head();
 nav();
-about();
-why();
-$db   = new AdminFaqModel();
-$faqs = $db->findAll(); 
-faq($faqs);
-//contact();
+stub();
 foot();
 
+function stub()
+{
+?>
+  <section id="about">
+    <br><br>
+    <div class="container">
+      <div class="center">
+          <hr>					
+        <br><br>
+        <div class="col-md-10 col-md-offset-1">
+          <h2>Dixie's Jukebox</h2>
+          <p class="lead" style="cursor: pointer;">
+          <a href="javascript:onclick=playSound(this, 'http://thepeoplesjukebox.com/mp3player/mp3player.php?jukebox=1234');">
+          <img style="cursor: pointer;" src="/images/jukebox.jpeg" border="0" id="jukebox"/></a><br>
+          <a href="javascript:ontouchstart=playSound(this, 'http://thepeoplesjukebox.com/mp3player/mp3player.php?jukebox=1234');">
+          <button id="but" type="submit" class="btn btn-primary btn-lg" style="cursor:pointer" >Play Me!</button>
+          </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php
+    
+}
 ?>
 
 
