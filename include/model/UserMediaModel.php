ad4002c58ab85fa13bb22e3213a50210e1223ca4
thatdixie <cyrusface@gmail.com>
2018-10-08 19:44:55 -0400

    Added UserMediaModel.php

---
+++ include/model/UserMediaModel.php
@@ -0,0 +1,40 @@
+<?php
+require_once "JukeboxDB.php";
+require      "Media.php";
+
+/********************************************************************
+ * UserMediaModel inherits MediaModel and provides functions to
+ * map User Media data. 
+ *
+ * @author  megan
+ * @version 181008
+ *********************************************************************
+ */
+class UserMediaModel extends MediaModel
+{
+    /*********************************************************
+     * Returns a Media by userId
+     *
+     * @return media
+     *********************************************************
+     */
+    public function findByUserId($id)
+    {
+        $query="SELECT mediaId,".
+                      "userId,".
+                      "mediaFile,".
+                      "mediaArtist,".
+                      "mediaTitle,".
+                      "mediaYear,".
+                      "mediaDuration,".
+                      "mediaCreated,".
+                      "mediaModified,".
+                      "mediaStatus ".                      		               
+	       "FROM media ".
+	       "WHERE mediaStatus='ACTIVE' AND userId=".$id;
+
+        return($this->selectDB($query, "Media"));
+    }
+}
+
+?>
