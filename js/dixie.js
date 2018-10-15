815d831cc0faa0772377f7be6a1ae7770ad6abd2
thatdixie <cyrusface@gmail.com>
2018-10-15 14:10:51 -0400

    Fixed bug in mp3player.php that broke iphone playback

--- js/dixie.js
+++ js/dixie.js
@@ -1,30 +1,24 @@
 
-function playSound(el,soundfile)
-{
-    
-    soundfile = soundfile+'&bust='+new Date().getTime();
-    if (el.mp3)
-    {
-        if (el.mp3.paused)
-	{
+function playSound(el,soundfile) {
+    if (el.mp3) {
+        if (el.mp3.paused) {
            //el.mp3 = new Audio(soundfile);
             el.mp3.load();
             el.mp3.play();
 	}
-        else if (el.mp3.ended)
-	{
-            el.mp3.load();
+        else if (el.mp3.ended) {
+            //el.mp3.load();
             el.mp3.play();
 	}
-        else
-	{
+        else {
             el.mp3.pause();
 	}
     }
-    else
-    {   
+    else {   
+    soundfile = soundfile+'&bust='+new Date().getTime();
         el.mp3 = new Audio(soundfile);        
-        el.mp3.loop = false;
+        //el.mp3.loop = false;
+        //el.mp3.load();
         el.mp3.play();
     }
 }
