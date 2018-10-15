25df4721efcaead4b9fa8e8591f8fedfe514a3f6
thatdixie <cyrusface@gmail.com>
2018-10-15 18:26:43 -0400

    Fixed extra style tag in nav view

--- include/view/page/index/nav.php
+++ include/view/page/index/nav.php
@@ -8,7 +8,7 @@
 function nav()
 {
 ?>
-    <nav class="navbar navbar-default navbar-fixed-top" style="background-color: black" style="color: white">
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color: black; color: white">
       <div class="container">
         <div class="row">
           <div class="col-md-4">
@@ -18,13 +18,13 @@ function nav()
           </div>					  
 
           <div class="col-md-8">	 
-            <div class="navbar-header" style="background-color: black" style="color: white">
+            <div class="navbar-header" style="background-color: black; color: white">
               <button type="button" class="navbar-toggle"  data-toggle="collapse" data-target="#menu">
                 <i class="fa fa-bars"></i>
               </button>
             </div>
             <div class="collapse navbar-collapse" id="menu">
-              <ul class="nav navbar-nav navbar-right" style="background-color: black" style="color: white">
+              <ul class="nav navbar-nav navbar-right" style="background-color: black; color: white">
                 <li><a href="#why">What Why How?</a></li>
                 <li><a href="/user/">Be a Jukebox</a></li>
                 <li><a href="/user/">Find a Jukebox</a></li>
