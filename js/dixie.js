7174c54788a1ea109af59bf5dac258c0b508588e
thatdixie <cyrusface@gmail.com>
2018-10-17 17:44:36 -0400

    homepage standalone app and favicon support

--- js/dixie.js
+++ js/dixie.js
@@ -1,4 +1,22 @@
+//-------------------------------------------
+// keeps standalone web-app from launching
+// new safari on links
+//-------------------------------------------
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+	window.location=this.getAttribute("href");
+	return false
+    }
+}
 
+
+//-------------------------------------------
+// This function plays the jukebox -- it's
+// a work in progres lol 
+//-------------------------------------------
 function playSound(el,soundfile) {
     if (el.mp3) {
         if (el.mp3.paused) {
