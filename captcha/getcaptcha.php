b8b944d5a1ee79f0f6cb339fafaaf777800879c4
thatdixie <cyrusface@gmail.com>
2018-10-20 06:56:03 -0400

    fixed bug in signup

--- captcha/getcaptcha.php
+++ captcha/getcaptcha.php
@@ -3,6 +3,8 @@
 // load CAPTCHA page
 //----------------------------------
 require_once "../include/view/page/captcha/indexIncludeFiles.php";
+require_once "../include/etc/session.php";
+siteSession();
 
 head();
 nav();
