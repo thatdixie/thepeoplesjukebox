1a67869d6c794b639fcd1ac5087e8ad261453fa6
thatdixie <cyrusface@gmail.com>
2019-01-12 22:57:59 -0500

    upload_catalog fix

--- login/index.php
+++ login/index.php
@@ -1,206 +1,28 @@
 <?php
-//---------------------------------
-// load admin/page
-//----------------------------------
-require_once "../include/view/page/admin/indexIncludeFiles.php";
-require_once "../include/etc/session.php";
-require_once "../include/controller/admin/admin.php";
-siteSession();
+/***********************************
+ * REST login for TPJ
+ *
+ * POST to:
+ * http://ThePeoplesJukebox/api/login/
+ *
+ * REST PARAMS:
+ *   username
+ *   password
+ *
+ * @author  dixie
+ ***********************************
+*/
+require_once "UserSession.php";
 
-if(isAdminLoginOK())
-{
-    $func  = getRequest('func');
-    $id    = getRequest('id');
+$u     = new UserSession();
     
-    if($func == "view_contact")
-    {
-        adminContact($id);
-    }
-    elseif($func =="mark_as_read")
-    {
-        markAsRead($id);
-    }
-    elseif($func =="mark_as_unread")
-    {
-        markAsUnread($id);        
-    }
-    elseif($func =="delete")
-    {
-        if(getRequest('confirm') =="yes")
-            deleteContact($id);
-        else
-            deleteConfirm($id);
-    }
-    elseif($func =="findcontacts")
-    {
-            adminFindContacts();
-    }
-    elseif($func =="logout")
-    {
-        adminLogout();
-        redirect("/login/");
-    }
-    else
-    {
-        //---------------------------------------
-        // We're logged-in show admin home page
-        // (it's our contact inbox) 
-        //---------------------------------------
-        adminPage();
-    }
+if(($user = $u->login(getRequest('username'), getRequest('password'))))
+{
+    jsonResponse($user->makeJson());
 }
 else
 {
-    //------------------------------------------
-    // get username and password (if entered)
-    //------------------------------------------
-    $username = getRequest('username');
-    $password = getRequest('password');
-
-    if($username && $password)
-    {
-        //---------------------------------------
-	    // Check username and password 
-	    //---------------------------------------
-	    if(adminLogin($username, $password))
-            adminPage();
-        else
-	        loginPage();
-    }
-    else
-    {
-        loginPage();
-    }
-}
-
-//---------------------------------------
-// admin home page
-//---------------------------------------
-function adminPage()
-{
-    if(!hasPermission("canPublish")
-    && !hasPermission("canUserEdit")
-    && !hasPermission("canContentEdit"))
-    {
-        //-----------------------------------------
-        // You're not a site admin go to regular
-        // jukebox page or home page.
-        //-----------------------------------------
-        if(hasPermission("canJukeboxAdmin"))
-            redirect("/user/index.php?jukeboxId=".getUserSession("userId")."&func=player");
-        else
-            redirect("/");
-    }
-
-    require_once "../include/model/AdminContactModel.php";
-
-    $db       = new AdminContactModel();
-    $contacts = $db->findUnread();
-    
-    adminHome($contacts);
-}
-
-//---------------------------------------
-// We're not logged-in show login screen
-//---------------------------------------
-function loginPage()
-{
-    login();
-}
-
-//---------------------------------------
-// mark as read
-//---------------------------------------
-function markAsRead($id)
-{
-    require_once "../include/model/ContactModel.php";
-
-    $db       = new ContactModel();
-    $contacts = $db->find($id);
-    $contact  = $contacts[0];
-
-    $contact->contactStatus ='READ';
-    $db->update($contact);
-
-    adminContact($id);
-}
-
-//---------------------------------------
-// mark as UNread
-//---------------------------------------
-function markAsUnread($id)
-{
-    require_once "../include/model/ContactModel.php";
-
-    $db       = new ContactModel();
-    $contacts = $db->find($id);
-    $contact  = $contacts[0];
-
-    $contact->contactStatus ='UNREAD';
-    $db->update($contact);
-
-    adminContact($id);
-}
-
-//---------------------------------------
-// admin a contact page
-//---------------------------------------
-function adminContact($id)
-{
-    require_once "../include/model/ContactModel.php";
-
-    $db       = new ContactModel();
-    $contacts = $db->find($id);
-    $contact  = $contacts[0];
-    
-    viewAdminContact($contact);
-}
-
-
-//---------------------------------------
-// delete confirm
-//---------------------------------------
-function deleteConfirm($id)
-{
-    require_once "../include/model/ContactModel.php";
-
-    $db       = new ContactModel();
-    $contacts = $db->find($id);
-    $contact  = $contacts[0];
-    
-    viewDeleteConfirm($contact);
-}
-
-//---------------------------------------
-// delete contact
-//---------------------------------------
-function deleteContact($id)
-{
-    require_once "../include/model/ContactModel.php";
-
-    $db = new ContactModel();
-    $db->delete($id);
-
-    //dbface("Contact Deleted", "/admin/");
-    //----------------------------------------------
-    // decided just going back to admin is good :-)
-    //----------------------------------------------
-    adminPage();
-}
-
-//---------------------------------------
-// find contacts by date/name/whatever...
-//---------------------------------------
-function adminFindContacts()
-{
-    require_once "../include/model/AdminContactModel.php";
-
-    $search_key  = getRequest('search_key');
-
-    $db       = new AdminContactModel();
-    $contacts = $db->findBySearch($search_key);
-
-    findContacts($contacts);
+    jsonErrorResponse("404", "User Not Found");
 }
 
 ?>
---
+++ upload_catalog/index.php
@@ -0,0 +1,64 @@
+<?php
+/**********************************************
+ * Upload a Jukebox Catalog for TPJ
+ *
+ * POST to:
+ * http://ThePeoplesJukebox/api/upload_catalog/
+ *
+ * REST PARAMS:
+ *   username
+ *   passcode
+ *   metadata
+ *   isUpload (optional)
+ *
+ * @author  dixie
+ **********************************************
+*/
+require_once "../login/UserSession.php";
+
+$u     = new UserSession();
+    
+if(($user = $u->getUserSession(getRequest('username'), getRequest('passcode'))))
+{
+    if($user[0]->userIsJukebox == 'YES')
+    {
+        //-------------------------------------------------------
+        // This will save upload metadata to upload table
+        // where a seperate process will insert new media records
+        // ------------------------------------------------------
+        $db     = new UserUploadModel();
+        $upload = $db->findByUserId($user[0]->userId);
+        
+        $upload[0]->uploadMetaData= getRequest('metadata');
+        $upload[0]->uploadStatus  = 'UPLOAD';
+        $upload[0]->uploadModified= sqlNow();
+        if(getRequest('isUpload') =='YES')
+            $upload[0]->uploadSource= 'UPLOAD';
+        else
+            $upload[0]->uploadSource= 'LOCAL';
+        $db->update($upload[0]);
+
+        //-----------------------------------------------------
+        // this will execute processUpload.php in background
+        // we call it with argv[1]=jukeboxId argv[2]=doc root
+        // ----------------------------------------------------
+        $root   = realpath($_SERVER["DOCUMENT_ROOT"]);
+        $script = $root."/scripts/processUpload.php";
+        exec("php ".$script."  ".$user[0]->userId." ".$root." > /opt/data/jukebox/upload.log");
+
+        //-----------------------------------
+        // return positive JSON response
+        //-----------------------------------
+        jsonResponse($upload[0]->makeJSON());
+    }
+    else
+    {
+        jsonErrorResponse("404", "User Not a Jukebox");
+    }
+}
+else
+{
+    jsonErrorResponse("404", "User Not Found");
+}
+
+?>
