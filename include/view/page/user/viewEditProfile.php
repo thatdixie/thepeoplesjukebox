1e3fd237b3d815aeb2e3fe17dbd749f59f33510a
thatdixie <cyrusface@gmail.com>
2020-04-17 16:54:10 -0400

    revert page/user/viewEditProfile.php

--- include/view/page/user/viewEditProfile.php
+++ include/view/page/user/viewEditProfile.php
@@ -30,95 +30,101 @@ function viewEditProfile($profile)
 ?>
   <section id="about">
     <div class="container">
-      <div class="center"><br>
-        <div class="row">
-          <form method="post" action="/user/index.php" enctype="multipart/form-data">
-            <div class="col-sm-3 col-offset-0">
-              <div class="fileinput fileinput-new" data-provides="fileinput">
-                <div class="fileinput-preview thumbnail" data-trigger="fileinput" style="width: 256px; height: 256px;">
-                  <?php echo "<img src=\"/user/photoviewer.php?userId=".$jukeboxId."\">"; ?>
-                </div>
-                <div>
-                  <span class="btn btn-primary btn-file">
-                    <span class="fileinput-new">Select image to change</span>
-                    <span class="fileinput-exists">Change</span>
-                    <input type="file" name="file">
-                  </span>
-                  <a href="#" class="btn btn-primary fileinput-exists" data-dismiss="fileinput">Remove</a>
-                </div>
-              </div>
+    <br><br>
+      <div class="row">
+      <form method="post" action="/user/index.php" enctype="multipart/form-data">
+        <div class="col-md-3 col-md-offset-1">
+          <!-- <label>Photo:</label> -->
+          <div class="fileinput fileinput-new" data-provides="fileinput">
+            <div class="fileinput-preview thumbnail" data-trigger="fileinput" style="width: 256px; height: 256px;">
+            <?php echo "<img src=\"/user/photoviewer.php?userId=".$jukeboxId."\">"; ?>
             </div>
-            <div class="col-8">
-              <h3>
-                <table>
-                  <tr>
-                    <td style="color: black"><b>Username:</b></td>
-                    <td style="color: black"><?php echo $profile[0]->userName;  ?></td>
-                  </tr>
-                  <tr>
-                  </tr>
-                  <tr>
-                    <td style="color: black"><b>First Name:</b></td>
-                    <td>
-                      <input type="text" name="first" value="<?php echo $profile[0]->userFirstName;  ?>" >
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="color: black"><b>Last Name:</b></td>
-                    <td>
-                      <input type="text" name="last" value="<?php echo $profile[0]->userLastName;  ?>" >
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="color: black"><b>Nickname:</b></td>
-                    <td>
-                      <input type="text" name="nickname" value="<?php echo $profile[0]->userNickName; ?>" >
-                    </td>
-                  </tr>
-                  <tr>
-                    <td><hr></td>
-                    <td><hr></td>
-                  </tr>
-                  <tr>
-                    <td style="color: black"><b>Be a Jukebox:</b></td>
-                    <td>&nbsp;
-                      <label class="radio-inline" style="color: black"><input type="radio" name="jukebox" value="NO" <?php echo $nocheck;  ?> >NO</label>
-                      <label class="radio-inline" style="color: black"><input type="radio" name="jukebox" value="YES" <?php echo $yescheck; ?> >YES</label>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="color: black"><b>Likes:</b></td>
-                    <td>
-                      <input type="text" name="likes" value="<?php echo $profile[0]->userLikes; ?>" >
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="color: black"><b>Where:</b></td>
-                    <td>
-                      <input type="text" name="where" value="<?php echo $profile[0]->userWorkplace; ?>" >
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="color: black"><b>When:</b></td>
-                    <td>
-                      <input type="text" name="when" value="<?php echo $profile[0]->userWorkHours; ?>" >
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;
-                      <input type="hidden" name="func" value="update_profile">
-                      <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Update Profile</button>
-                    </td>
-		  </tr>
-                </table>
-              </h3>
+            <div>
+              <span class="btn btn-default btn-file">
+                <span class="fileinput-new">Select image to change</span>
+                <span class="fileinput-exists">Change</span>
+                <input type="file" name="file">
+              </span>
+              <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
             </div>
-          </form>
-        </div> <!-- end row -->
-      </div>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <h3>
+          <table>
+            <tr>
+              <td><b>Username:</b></td>
+              <td style="color: black">
+              <?php echo $profile[0]->userName;  ?>
+              </td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td><b>First Name:</b></td>
+              <td>
+              <input type="text" name="first" value="<?php echo $profile[0]->userFirstName;  ?>" >
+              </td>
+            </tr>
+            <tr>
+              <td><b>Last Name:</b></td>
+              <td>
+                <input type="text" name="last" value="<?php echo $profile[0]->userLastName;  ?>" >
+              </td>
+            </tr>
+            <tr>
+              <td><b>Nickname:</b></td>
+              <td>
+                <input type="text" name="nickname" value="<?php echo $profile[0]->userNickName; ?>" >
+              </td>
+            </tr>
+            <tr>
+              <td><hr></td>
+              <td><hr></td>
+            </tr>  
+            <tr>
+              <td><b>Be a Jukebox:</b></td>
+              <td>
+              &nbsp;
+              <label class="radio-inline"><input type="radio" name="jukebox" value="NO"  <?php echo $nocheck;  ?> >NO</label>
+              <label class="radio-inline"><input type="radio" name="jukebox" value="YES" <?php echo $yescheck; ?> >YES</label>
+              </td>
+            </tr>
+            <tr>
+              <td><b>Likes:</b></td>
+              <td>
+                <input type="text" name="likes" value="<?php echo $profile[0]->userLikes; ?>" >
+              </td>
+            </tr>
+            <tr>
+              <td><b>Where:</b></td>
+              <td>
+                <input type="text" name="where" value="<?php echo $profile[0]->userWorkplace; ?>" >
+              </td>
+            </tr>
+            <tr>
+              <td><b>When:</b></td>
+              <td>
+                <input type="text" name="when" value="<?php echo $profile[0]->userWorkHours; ?>" >
+              </td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td>
+                &nbsp;
+                <input type="hidden" name="func" value="update_profile">
+                <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Update Profile</button>              
+              </td>
+            </tr>
+          </table>
+          </h3>  
+        </div>
+      </form>
+      </div> <!-- end row -->
       <hr>
-    </div><!-- end container -->
+    </div> <!-- end container -->
   </section>
 <?php
 
@@ -126,3 +132,6 @@ function viewEditProfile($profile)
 }
 
 ?>
+
+
+
